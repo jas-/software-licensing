deb9a0dbb7d6d390f5ff3a412d5edfde0f98a072
jas- <jason.gerfen@gmail.com>
2012-01-23 13:36:02 -0700

    Added some CSRF and header features;

--- index.php
+++ index.php
@@ -8,14 +8,23 @@ if (!file_exists(__SITE.'/controllers/controller.initialization.php')){
 }
 include __SITE.'/controllers/controller.initialization.php';
 
+/* intialize the libraries */
+$registry->libs = libraries::init();
+
+/* generate or use CSRF token */
+$token = (!empty($_SESSION['csrf'])) ? $_SESSION['csrf'] : $registry->libs->uuid();
+
+/* apply customized headers */
+header('X-Alt-Referer: '.$token);
+header('X-Forwarded-Proto: http');
+header('X-Frame-Options: deny');
+header('X-XSS-Protecton: 1;mode=deny');
+
 /* load the router via the registry */
 $registry->router = new router($registry);
 
 /* route requests through controllers */
 $registry->router->setPath(__SITE.'/controllers');
-
-/* intialize the libraries */
-$registry->libs = libraries::init();
 
 /* load the template via the registry */
 $registry->template = new template($registry);
