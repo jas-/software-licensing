77a6df1777050478aab00ca0794c514428a4cd8c
jas- <jason.gerfen@gmail.com>
2012-01-22 19:00:38 -0700

    Moved primary template loading to view

--- controllers/controller.index.php
+++ controllers/controller.index.php
@@ -11,38 +11,15 @@ class indexController
  public function __construct($registry)
  {
   $this->registry = $registry;
-  $this->registry->tpl = new templates();
-  $this->registry->tpl->strTemplateDir = $this->registry->opts['template'];
-  $this->registry->tpl->boolCache=true;
  }
 
  public function index()
  {
-  $this->_header();
-  $this->_main();
-  $this->_footer();
- }
-
- private function _header()
- {
-  $this->registry->tpl->assign('title',
-                               $this->registry->opts['title'], NULL, NULL);
-  $this->registry->tpl->assign('timeout',
-                               $this->registry->opts['timeout'], NULL, NULL);
-  $this->registry->tpl->assign('templates',
-                               $this->registry->tpl->strTemplateDir, NULL, NULL);
-  $this->registry->tpl->display('header.tpl', true, NULL,
-                                $this->registry->libs->_getRealIPv4());
- }
-
- private function _main()
- {
-  
- }
-
- private function _footer()
- {
-  $this->registry->tpl->display('footer.tpl', true, NULL, $this->registry->libs->_getRealIPv4());
+  if (file_exists('views/view.index.php')){
+   require 'views/view.index.php';
+  }
+  $index = new indexView($this->registry);
+  $index->index();
  }
 }
 ?>
