f461645bb348a83494a629578585b91c71c47d96
jas- <jason.gerfen@gmail.com>
2012-02-24 10:35:10 -0700

    Fri Feb 24 10:35:10 MST 2012 - Added new SP to handle retrieval of authenticated users access level and group membership

--- install/sp_authentication.sql
+++ install/sp_authentication.sql
@@ -9,4 +9,13 @@ BEGIN
  SELECT COUNT(*) AS x FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(challenge))=email AND AES_DECRYPT(BINARY(UNHEX(password)), SHA1(challenge))=pword;
 END//
 
+DROP PROCEDURE IF EXISTS Auth_GetLevelGroup//
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Auth_GetLevelGroup(IN `email` VARCHAR(128), IN `sKey` LONGTEXT)
+ DETERMINISTIC
+ SQL SECURITY INVOKER
+ COMMENT 'Retrieves the users access level and group membership'
+BEGIN
+ SELECT `level`,`group` FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(sKey))=email;
+END//
+
 DELIMITER ;
