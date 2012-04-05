b7171c152949f9509029e4bb0a071aa12f46ca81
jas- <jason.gerfen@gmail.com>
2012-04-05 10:40:03 -0600

    Thu Apr  5 10:40:03 MDT 2012 - Modified management of ACL stored procedures

--- install/stored-procedures/sp_configuration_access.sql
+++ install/stored-procedures/sp_configuration_access.sql
@@ -1,12 +1,31 @@
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS Configuration_access_add//
-CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_access_add(IN `allow` VARCHAR(30), IN `deny` VARCHAR(30), `skey` LONGTEXT)
+DROP PROCEDURE IF EXISTS Configuration_access_add_allow//
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_access_add_allow(IN `allow` LONGTEXT, `skey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
- COMMENT 'Add or updates access control lists'
+ COMMENT 'Add or updates allowed access control lists'
 BEGIN
- INSERT INTO `configuration_access` (`allow`, `deny`) VALUES (HEX(AES_ENCRYPT(allow, SHA1(skey))), HEX(AES_ENCRYPT(deny, SHA1(skey)))) ON DUPLICATE KEY UPDATE `allow`=HEX(AES_ENCRYPT(allow, SHA1(skey))), `deny`=HEX(AES_ENCRYPT(deny, SHA1(skey)));
+ IF EXISTS (SELECT `allow` FROM `configuration_access` WHERE `allow`=HEX(AES_ENCRYPT(allow, SHA1(skey))))
+ THEN
+  UPDATE `configuration_access` SET `allow`=HEX(AES_ENCRYPT(allow, SHA1(skey))) WHERE `id`=id LIMIT 1;
+ ELSE
+  INSERT INTO `configuration_access` (`allow`) VALUES (HEX(AES_ENCRYPT(allow, SHA1(skey)))) ON DUPLICATE KEY UPDATE `allow`=HEX(AES_ENCRYPT(allow, SHA1(skey)));
+ END IF;
+END//
+
+DROP PROCEDURE IF EXISTS Configuration_access_add_deny//
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_access_add_deny(IN `deny` LONGTEXT, `skey` LONGTEXT)
+ DETERMINISTIC
+ SQL SECURITY INVOKER
+ COMMENT 'Add or updates denied access control lists'
+BEGIN
+ IF EXISTS (SELECT `deny` FROM `configuration_access` WHERE `deny`=HEX(AES_ENCRYPT(deny, SHA1(skey))))
+ THEN
+  UPDATE `configuration_access` SET `deny`=HEX(AES_ENCRYPT(deny, SHA1(skey))) WHERE `id`=id LIMIT 1;
+ ELSE
+  INSERT INTO `configuration_access` (`deny`) VALUES (HEX(AES_ENCRYPT(deny, SHA1(skey)))) ON DUPLICATE KEY UPDATE `deny`=HEX(AES_ENCRYPT(deny, SHA1(skey)));
+ END IF;
 END//
 
 DROP PROCEDURE IF EXISTS Configuration_access_del//
@@ -18,13 +37,14 @@ BEGIN
  DELETE FROM `configuration_access` WHERE `id`=id LIMIT 1;
 END//
 
+DELIMITER //
 DROP PROCEDURE IF EXISTS Configuration_access_get//
-CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_access_get(IN `skey` LONGTEXT)
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_access_get(IN `type` VARCHAR(30), IN `skey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Returns list of access controls'
 BEGIN
- SELECT AES_DECRYPT(BINARY(UNHEX(allow)), SHA1(sKey)) AS allow, AES_DECRYPT(BINARY(UNHEX(deny)), SHA1(sKey)) AS deny FROM `configuration_access`;
+ SELECT AES_DECRYPT(BINARY(UNHEX(type)), SHA1(sKey)) AS type FROM `configuration_access` WHERE type != "";
 END//
 
 DELIMITER ;
