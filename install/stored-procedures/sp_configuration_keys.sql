c5535186e65b5e2cebbc7ecc92c15f944f6109bc
jas- <jason.gerfen@gmail.com>
2012-04-20 13:58:38 -0600

    Fri Apr 20 13:58:38 MDT 2012 - Added affected rows on keys and users stored procedures for success/fail checks

--- install/stored-procedures/sp_configuration_keys.sql
+++ install/stored-procedures/sp_configuration_keys.sql
@@ -6,7 +6,10 @@ CREATE DEFINER='licesning'@'localhost' PROCEDURE Configuration_keys_add(IN `coun
  SQL SECURITY INVOKER
  COMMENT 'Add or updates users key pair'
 BEGIN
+ SET foreign_key_checks = 0;
  INSERT INTO `configuration_openssl_keys` (`countryName`, `stateOrProvinceName`, `localityName`, `organizationName`, `organizationalUnitName`, `commonName`, `emailAddress`, `privateKey`, `publicKey`) VALUES (HEX(AES_ENCRYPT(countryName, SHA1(sKey))), HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), HEX(AES_ENCRYPT(localityName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), HEX(AES_ENCRYPT(commonName, SHA1(sKey))), HEX(AES_ENCRYPT(emailAddress, SHA1(sKey))), HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), HEX(AES_ENCRYPT(publicKey, SHA1(sKey)))) ON DUPLICATE KEY UPDATE `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddress, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey)));
+ SET foreign_key_checks = 1;
+ SELECT ROW_COUNT() AS affected;
 END//
 
 DROP PROCEDURE IF EXISTS Configuration_keys_get//
