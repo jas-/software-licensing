1997ea99ab0ca29f61ef64c7fead1cb90b1b52c8
jas- <jason.gerfen@gmail.com>
2012-04-27 11:26:06 -0600

    Fri Apr 27 11:26:05 MDT 2012 - Resolved problem with new keyring additions regarding stored procedure use

--- install/stored-procedures/sp_configuration_keys.sql
+++ install/stored-procedures/sp_configuration_keys.sql
@@ -1,25 +1,19 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS Configuration_keys_add//
-CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_keys_add(IN `countryName` VARCHAR(64), IN `stateOrProvinceName` VARCHAR(64), IN `localityName` VARCHAR(64), IN `organizationName` VARCHAR(64), IN `organizationalUnitName` VARCHAR(64), IN `commonName` VARCHAR(64), IN `emailAddress` VARCHAR(64), IN `privateKey` LONGTEXT, IN `publicKey` LONGTEXT, IN `sKey` LONGTEXT)
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_keys_add(IN `countryName` VARCHAR(64), IN `stateOrProvinceName` VARCHAR(64), IN `localityName` VARCHAR(64), IN `organizationName` VARCHAR(64), IN `organizationalUnitName` VARCHAR(64), IN `commonName` VARCHAR(64), IN `emailAddy` VARCHAR(64), IN `privateKey` LONGTEXT, IN `publicKey` LONGTEXT, IN `sKey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Add or updates users key pair'
 BEGIN
- DECLARE x INT DEFAULT 0;
- DECLARE affected INT DEFAULT 0;
- SET foreign_key_checks = 0;
-
- SELECT COUNT(*) INTO x FROM `configuration_openssl_keys` WHERE AES_DECRYPT(BINARY(UNHEX(emailAddress)), SHA1(sKey))=emailAddress;
- IF x > 0
+ DECLARE affected INT;
+ IF (SELECT COUNT(*) FROM `configuration_openssl_keys` WHERE AES_DECRYPT(BINARY(UNHEX(emailAddress)), SHA1(sKey))=emailAddy > 0)
  THEN
-  UPDATE `configuration_openssl_keys` SET `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddress, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey))) WHERE HEX(AES_ENCRYPT(emailAddress, SHA1(sKey)))=emailAddress;
+  UPDATE `configuration_openssl_keys` SET `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey))) WHERE HEX(AES_ENCRYPT(emailAddress, SHA1(sKey)))=emailAddy;
  ELSE
-  INSERT INTO `configuration_openssl_keys` (`countryName`, `stateOrProvinceName`, `localityName`, `organizationName`, `organizationalUnitName`, `commonName`, `emailAddress`, `privateKey`, `publicKey`) VALUES (HEX(AES_ENCRYPT(countryName, SHA1(sKey))), HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), HEX(AES_ENCRYPT(localityName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), HEX(AES_ENCRYPT(commonName, SHA1(sKey))), HEX(AES_ENCRYPT(emailAddress, SHA1(sKey))), HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), HEX(AES_ENCRYPT(publicKey, SHA1(sKey)))) ON DUPLICATE KEY UPDATE `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddress, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey)));
+  INSERT INTO `configuration_openssl_keys` (`countryName`, `stateOrProvinceName`, `localityName`, `organizationName`, `organizationalUnitName`, `commonName`, `emailAddress`, `privateKey`, `publicKey`) VALUES (HEX(AES_ENCRYPT(countryName, SHA1(sKey))), HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), HEX(AES_ENCRYPT(localityName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), HEX(AES_ENCRYPT(commonName, SHA1(sKey))), HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), HEX(AES_ENCRYPT(publicKey, SHA1(sKey)))) ON DUPLICATE KEY UPDATE `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey)));
  END IF;
-
- SET foreign_key_checks = 1;
- SELECT ROW_COUNT() AS affected;
+ SELECT LAST_INSERT_ID() INTO affected;
 END//
 
 DROP PROCEDURE IF EXISTS Configuration_keys_get//
