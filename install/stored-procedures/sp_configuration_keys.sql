3f714173f4daa5378c4afad0245ece4230edddae
jas- <jason.gerfen@gmail.com>
2012-04-27 11:58:30 -0600

    Fri Apr 27 11:58:30 MDT 2012 - Bug with return value of stored procedure to add/update keyring data per user

--- install/stored-procedures/sp_configuration_keys.sql
+++ install/stored-procedures/sp_configuration_keys.sql
@@ -6,14 +6,13 @@ CREATE DEFINER='licensing'@'localhost' PROCEDURE Configuration_keys_add(IN `coun
  SQL SECURITY INVOKER
  COMMENT 'Add or updates users key pair'
 BEGIN
- DECLARE affected INT;
  IF (SELECT COUNT(*) FROM `configuration_openssl_keys` WHERE AES_DECRYPT(BINARY(UNHEX(emailAddress)), SHA1(sKey))=emailAddy > 0)
  THEN
   UPDATE `configuration_openssl_keys` SET `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey))) WHERE HEX(AES_ENCRYPT(emailAddress, SHA1(sKey)))=emailAddy;
  ELSE
   INSERT INTO `configuration_openssl_keys` (`countryName`, `stateOrProvinceName`, `localityName`, `organizationName`, `organizationalUnitName`, `commonName`, `emailAddress`, `privateKey`, `publicKey`) VALUES (HEX(AES_ENCRYPT(countryName, SHA1(sKey))), HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), HEX(AES_ENCRYPT(localityName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), HEX(AES_ENCRYPT(commonName, SHA1(sKey))), HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), HEX(AES_ENCRYPT(publicKey, SHA1(sKey)))) ON DUPLICATE KEY UPDATE `countryName`=HEX(AES_ENCRYPT(countryName, SHA1(sKey))), `stateOrProvinceName`=HEX(AES_ENCRYPT(stateOrProvinceName, SHA1(sKey))), `localityName`=HEX(AES_ENCRYPT(localityName, SHA1(sKey))), `organizationName`=HEX(AES_ENCRYPT(organizationName, SHA1(sKey))), `organizationalUnitName`=HEX(AES_ENCRYPT(organizationalUnitName, SHA1(sKey))), `commonName`=HEX(AES_ENCRYPT(commonName, SHA1(sKey))), `emailAddress`=HEX(AES_ENCRYPT(emailAddy, SHA1(sKey))), `privateKey`=HEX(AES_ENCRYPT(privateKey, SHA1(sKey))), `publicKey`=HEX(AES_ENCRYPT(publicKey, SHA1(sKey)));
  END IF;
- SELECT LAST_INSERT_ID() INTO affected;
+ SELECT ROW_COUNT() AS affected;
 END//
 
 DROP PROCEDURE IF EXISTS Configuration_keys_get//
