b076e3c68d6c6935e8b1d2cc6425a9da1eadfa27
jas- <jason.gerfen@gmail.com>
2012-05-01 10:13:59 -0600

    Tue May  1 10:13:59 MDT 2012 - Fixed bug with stored procedure used to perform authentication

--- install/stored-procedures/sp_authentication.sql
+++ install/stored-procedures/sp_authentication.sql
@@ -1,12 +1,12 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS Auth_CheckUser//
-CREATE DEFINER='licensing'@'localhost' PROCEDURE Auth_CheckUser(IN `email` VARCHAR(128), IN `pword` LONGTEXT, IN `challenge` LONGTEXT)
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Auth_CheckUser(IN `emailAddy` VARCHAR(128), IN `pword` LONGTEXT, IN `challenge` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Performs authentication check'
 BEGIN
- SELECT COUNT(*) AS x FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(challenge))=email AND AES_DECRYPT(BINARY(UNHEX(password)), SHA1(challenge))=pword;
+ SELECT COUNT(*) AS x FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(challenge))=emailAddy AND AES_DECRYPT(BINARY(UNHEX(password)), SHA1(challenge))=pword;
 END//
 
 DROP PROCEDURE IF EXISTS Auth_GetLevelGroup//
