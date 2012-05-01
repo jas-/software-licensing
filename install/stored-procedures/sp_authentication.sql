b7fcc339ed7907622e0ed123967320814161b2c1
jas- <jason.gerfen@gmail.com>
2012-05-01 12:14:36 -0600

    Tue May  1 12:14:36 MDT 2012 - Token generation upong successful login complete. Comprised of user specific data; username, access level, group membership, anti- session hijacking data & timeout fields are all AES encrypted based on user supplied credentials

--- install/stored-procedures/sp_authentication.sql
+++ install/stored-procedures/sp_authentication.sql
@@ -10,12 +10,12 @@ BEGIN
 END//
 
 DROP PROCEDURE IF EXISTS Auth_GetLevelGroup//
-CREATE DEFINER='licensing'@'localhost' PROCEDURE Auth_GetLevelGroup(IN `email` VARCHAR(128), IN `sKey` LONGTEXT)
+CREATE DEFINER='licensing'@'localhost' PROCEDURE Auth_GetLevelGroup(IN `emailAddy` VARCHAR(128), IN `sKey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Retrieves the users access level and group membership'
 BEGIN
- SELECT `level`,`group` FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(sKey))=email;
+ SELECT AES_DECRYPT(BINARY(UNHEX(level)), SHA1(sKey)) AS level, AES_DECRYPT(BINARY(UNHEX(authentication.group)), SHA1(sKey)) AS grp FROM `authentication` WHERE AES_DECRYPT(BINARY(UNHEX(email)), SHA1(sKey))=emailAddy;
 END//
 
 DELIMITER ;
