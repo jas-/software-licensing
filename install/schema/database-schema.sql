598549d3dc13715026d752d7de82afb9d61e37e6
jas- <jason.gerfen@gmail.com>
2012-06-12 12:50:48 -0600

    Tue Jun 12 12:50:48 MDT 2012 - EOD

--- install/schema/database-schema.sql
+++ install/schema/database-schema.sql
@@ -120,8 +120,9 @@ DROP TABLE IF EXISTS `configuration_applications`;
 CREATE TABLE IF NOT EXISTS `configuration_applications` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `resource` varchar(128) NOT NULL,
-  `application` VARCHAR(255) DEFAULT NULL,
-  `url` VARCHAR(255) DEFAULT NULL,
+  `application` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `ip` LONGTEXT, NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `resource` (`resource`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
